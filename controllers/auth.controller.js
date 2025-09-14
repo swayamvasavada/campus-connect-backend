a6b2f39bc999ae46414e70db8b4a62d70d291625
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2025-09-14 11:55:44 +0530

    Added logic for password reset

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,13 +2,14 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 const validators = require('../util/validators');
+const mailTemplete = require('../util/mail-templete');
 const jwt = require('../util/token');
 
 async function signup(req, res, next) {
     try {
         const enteredData = req.body;
         console.log("Entered data: ", enteredData);
-        
+
         // Validation
         if (!validators.validateSignupInput(enteredData)) {
             return res.status(400).json({ hasError: true, message: "Please fill all details properly" });
@@ -28,7 +29,11 @@ async function signup(req, res, next) {
         });
         await newUser.save();
 
-        const token = jwt.signToken({ id: newUser._id });
+        const token = jwt.signToken({ id: newUser._id }, true);
+        await User.findOneAndUpdate({ email: enteredData.email }, { previousLogin: user.lastLogin, lastLogin: new Date() });
+
+        mailTemplete.welcomeEmail(newUser.email, newUser.name);
+
         return res.status(201).json({ user: newUser, token });
     } catch (error) {
         next(error);
@@ -38,7 +43,7 @@ async function signup(req, res, next) {
 async function login(req, res, next) {
     try {
         const enteredData = req.body;
-        
+
         // Validation
         if (!validators.validateLoginInput(enteredData)) {
             return res.status(400).json({ hasError: true, message: "Please fill all details properly" });
@@ -56,8 +61,71 @@ async function login(req, res, next) {
             return res.status(400).json({ hasError: true, message: "Seems like email or password is incorrect!" });
         }
 
-        const token = jwt.signToken({id: user._id});
-        res.json({user, token});
+        const token = jwt.signToken({ id: user._id }, true);
+        await User.findOneAndUpdate({ email: enteredData.email }, { previousLogin: user.lastLogin, lastLogin: new Date() });
+        res.json({ user, token });
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function requestReset(req, res, next) {
+    const email = req.body.email;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = email && emailRegex.test(email);
+
+    if (!isValidEmail) return res.status(400).json({
+        hasError: true,
+        message: 'Please enter a valid email!'
+    });
+
+    try {
+        const user = await User.findOne({ email: email });
+        if (!user) return res.status(404).json({
+            hasError: true,
+            message: 'User does not exist'
+        });
+
+        const token = jwt.signToken({ id: user._id });
+
+        mailTemplete.requestResetEmail(user.email, user.name, token);
+
+        res.json({
+            hasError: false,
+            message: "We have sent you reset link on your email"
+        });
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function resetPassword(req, res, next) {
+    const resetToken = req.params["resetToken"];
+    const enteredData = req.body;
+    if (!resetToken || resetToken === '') return res.status(400).json({
+        hasError: true,
+        message: "Reset token not found"
+    });
+
+    if (!validators.validatePasswordInput(enteredData.password)) return res.status(400).json({
+        hasError: true,
+        message: "Please fill all details properly!"
+    });
+
+    try {
+        const hashedPassword = await bcrypt.hash(enteredData.password, 12);
+
+        const id = jwt.verifyToken(resetToken).id;
+        const user = await User.findById(id);
+        await user.updateOne({ password: hashedPassword });
+        
+        mailTemplete.resetConfirmation(user.email, user.name);
+
+        return res.json({
+            hasError: false,
+            message: "Password updated successfully"
+        });
     } catch (error) {
         next(error);
     }
@@ -65,5 +133,7 @@ async function login(req, res, next) {
 
 module.exports = {
     signup: signup,
-    login: login
+    login: login,
+    requestReset: requestReset,
+    resetPassword: resetPassword
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,4 +5,8 @@ router.post('/signup', authController.signup);
 
 router.post('/login', authController.login);
 
+router.post('/request-reset', authController.requestReset);
+
+router.post('/reset-password/:resetToken', authController.resetPassword);
+
 module.exports = router;

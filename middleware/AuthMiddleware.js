cb46bbfef70c6ea64111b5cc245c891543fd95e1
Swayam Vasavada <swayam.vasavada1505@gmail.com>
2025-10-13 00:05:21 +0530

    Bug fixes

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -3,6 +3,18 @@ const jwt = require("../util/token");
 
 async function checkAuth(req, res, next) {
     try {
+        const excludedRoutes = ['/api/user/resend-activation-email', /^\/api\/user\/activate\/[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/] // Match /api/user/activate/{activateToken} (alphanumeric token)];  // Routes to exclude
+        console.log("Req: ", req.path);
+
+        for (let route of excludedRoutes) {
+            if (route instanceof RegExp && route.test(req.path)) {
+                return next();
+            }
+            if (route === req.path) {
+                return next();
+            }
+        }
+
         const authHeader = req.headers['authorization'];
         const token = authHeader?.split(' ')[1];
 
@@ -18,23 +30,12 @@ async function checkAuth(req, res, next) {
         // Setting user id for request
         res.locals.userId = data.id;
 
-        const excludedRoutes = ['/api/user/resend-activation-email', /^\/api\/user\/activate\/[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+\.[a-zA-Z0-9-_]+$/] // Match /api/user/activate/{activateToken} (alphanumeric token)];  // Routes to exclude
-        console.log("Req: ", req.path);
-
-        for (let route of excludedRoutes) {
-            if (route instanceof RegExp && route.test(req.path)) {
-                return next();
-            }
-            if (route === req.path) {
-                return next();
-            }
-        }
-
         const accountStatus = await User.findById(data.id, { isActivated: 1 });
 
         if (!accountStatus.isActivated) {
             return res.status(401).json({
                 hasError: true,
+                requiresActivation: true,
                 message: "Your email is not verified! Please verify your email"
             });
         }
